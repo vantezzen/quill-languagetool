d5e02bf63e36f61a58ed4caa8134d37d5e3f458e
vantezzen <hello@vantezzen.io>
2022-08-15 12:41:58 +0000

    Deploying to gh-pages from @ vantezzen/quill-languagetool@08828c16b0ee1872322ecb74d0aee7a15239d3d4 🚀

---
+++ precache-manifest.5ce4a9969e416e02f7655ff94917deea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dac2aae9c7bc1aac08b97ffaa94b72",
+    "url": "/quill-languagetool/index.html"
+  },
+  {
+    "revision": "fdd3545bdbf71d084032",
+    "url": "/quill-languagetool/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "c6d945aa3ee8ce024532",
+    "url": "/quill-languagetool/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "fdd3545bdbf71d084032",
+    "url": "/quill-languagetool/static/js/2.810f1264.chunk.js"
+  },
+  {
+    "revision": "39437e89a4c49cb6cd4ecef99b560105",
+    "url": "/quill-languagetool/static/js/2.810f1264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d945aa3ee8ce024532",
+    "url": "/quill-languagetool/static/js/main.4d10ea56.chunk.js"
+  },
+  {
+    "revision": "1e0ac042d79c1cdbecd5",
+    "url": "/quill-languagetool/static/js/runtime-main.8c8c23e1.js"
+  }
+]);
