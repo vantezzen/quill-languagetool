5a77105e5d29c73ae8c32024427c6caf3fcd16f7
Bennett Hollstein <hello@vantezzen.io>
2022-08-18 15:09:49 +0200

    Improve position of suggestionbox

--- src/PopupManager.ts
+++ src/PopupManager.ts
@@ -11,8 +11,10 @@ import { MatchesEntity } from "./types";
  */
 export default class PopupManager {
   private openPopup?: HTMLElement;
+  private currentSuggestionElement?: HTMLElement;
 
   constructor(private readonly parent: QuillLanguageTool) {
+    this.closePopup = this.closePopup.bind(this);
     this.addEventHandler();
   }
 
@@ -23,6 +25,21 @@ export default class PopupManager {
         this.handleSuggestionClick(target);
       }
     });
+
+    window.addEventListener("resize", () => {
+      if (this.currentSuggestionElement) {
+        this.handleSuggestionClick(this.currentSuggestionElement);
+      }
+    });
+  }
+
+  private closePopup() {
+    console.log("Closing popup", this.openPopup, this.currentSuggestionElement);
+    if (this.openPopup) {
+      this.openPopup.remove();
+      this.openPopup = undefined;
+    }
+    this.currentSuggestionElement = undefined;
   }
 
   private handleSuggestionClick(suggestion: HTMLElement) {
@@ -41,15 +58,11 @@ export default class PopupManager {
 
   private createSuggestionPopup(match: MatchesEntity, suggestion: HTMLElement) {
     if (this.openPopup) {
-      this.openPopup.remove();
+      this.closePopup();
     }
+    this.currentSuggestionElement = suggestion;
 
-    const suggestionPosition = suggestion.getBoundingClientRect();
-    const top = suggestionPosition.top + suggestionPosition.height;
-    const left = suggestionPosition.left;
-    let closePopup = () => {
-      document.body.removeChild(popup);
-    };
+    const popupPositionStyle = this.getIdealPopupPositionStyle(suggestion);
 
     const applySuggestion = (replacement: string) => {
       this.parent.preventLoop();
@@ -59,13 +72,16 @@ export default class PopupManager {
       // @ts-ignore
       this.parent.quill.setSelection(match.offset + replacement.length);
 
-      closePopup();
+      this.closePopup();
     };
 
     const popup = html`
-      <div class="quill-lt-match-popup" style="top:${top}px;left:${left}px;">
+      <div class="quill-lt-match-popup" style="${popupPositionStyle}">
         <div class="quill-lt-match-popup-header">
-          <button class="quill-lt-match-popup-close" onclick="${closePopup}">
+          <button
+            class="quill-lt-match-popup-close"
+            onclick="${this.closePopup}"
+          >
             ${raw("&times;")}
           </button>
         </div>
@@ -94,5 +110,40 @@ export default class PopupManager {
 
     document.body.appendChild(popup);
     this.openPopup = popup;
+  }
+
+  private getIdealPopupPositionStyle(suggestion: HTMLElement) {
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    const suggestionPosition = suggestion.getBoundingClientRect();
+    let popupPosition: { [item: string]: number } = {};
+
+    if (suggestionPosition.top > windowHeight / 2) {
+      popupPosition.bottom = windowHeight - suggestionPosition.top;
+    } else {
+      popupPosition.top = suggestionPosition.top + suggestionPosition.height;
+    }
+
+    if (suggestionPosition.left > 400) {
+      // Position  popup left of suggestion
+      popupPosition.right = windowWidth - suggestionPosition.right;
+    } else {
+      // Position popup right of suggestion
+      popupPosition.left = suggestionPosition.left;
+
+      if (!this.hasEnoughSpaceForPopup(popupPosition.left, windowWidth)) {
+        // If there is enough space to the right, position the popup centrally
+        popupPosition.left = Math.max(10, popupPosition.left - 200);
+      }
+    }
+    const popupPositionStyle = Object.keys(popupPosition)
+      .map((key) => `${key}: ${popupPosition[key]}px`)
+      .join(";");
+    return popupPositionStyle;
+  }
+
+  private hasEnoughSpaceForPopup(border: number, maximum: number) {
+    return maximum - border > 400;
   }
 }
