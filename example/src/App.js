d5996ec9b608e922940f3befbb58e75ba6b184cd
Larissa <larissa.tardivo@estantemagica.com.br>
2023-03-07 19:33:44 -0300

    feat: set default value

--- example/src/App.js
+++ example/src/App.js
@@ -17,11 +17,13 @@ const modules = {
   },
 };
 const App = () => {
-  const [value, setValue] = useState("");
+  const val = 'Jontus era um macaquinho muito intilijente. Ele gostava de explora a floresta e de descubrir coisas novas. Um dia, ele viu um bichinho no chão e correu até ele para pegalo. Mas quando chegou perto, viu que o bichinho era uma lagartixa muito assustada. Jontus ficou triste por ter assustado a lagartixa e decidiu se desculpar. Ele falou "descupa, eu não qui fazer mal. Eu só queria brincar". A lagartixa ficou mais calma e os dois se tornaram amigos.'
+  const [value, setValue] = useState(val);
+
   return (
     <ReactQuill
-      value={value}
-      onChange={setValue}
+      defaultValue={val}
+      onChange={(_,__,___,editor) => setValue(editor.getText())}
       modules={modules}
       formats={null}
     />
