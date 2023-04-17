950df2c92d31336e5a9b5ba68f280b66d9f60199
Larissa <larissa.tardivo@estantemagica.com.br>
2023-04-17 15:56:23 -0300

    fix: added listener to break line

--- example/src/App.js
+++ example/src/App.js
@@ -1,33 +1,42 @@
-import React, { useState } from 'react'
-import ReactQuill, { Quill } from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
-import registerQuillSpellChecker from 'react-quill-spell-checker'
+import React, { useState } from "react"
+import ReactQuill, { Quill } from "react-quill"
+import "react-quill/dist/quill.snow.css"
+import registerQuillSpellChecker from "react-quill-spell-checker"
 
 registerQuillSpellChecker(Quill)
 
 const addDisabledCategoriesOnBody = (text) => {
   const body = {
     text,
-    language: 'auto',
-    disabledCategories: 'FORMAL_SPEECH'
+    language: "auto",
+    disabledCategories: "FORMAL_SPEECH",
   }
   return Object.keys(body)
     .map((key) => `${key}=${encodeURIComponent(body[key])}`)
-    .join('&')
+    .join("&")
 }
 
 const modules = {
+  keyboard: {
+    bindings: {
+      enter: {
+        key: 13,
+        handler: function () {},
+      },
+    },
+  },
   spellChecker: {
     cooldownTime: process.env.REACT_APP_COOLDOWN_TIME,
     showLoadingIndicator: true,
     api: {
       url: process.env.REACT_APP_SPELL_CHECKER_URI,
-      body: addDisabledCategoriesOnBody
-    }
-  }
+      body: addDisabledCategoriesOnBody,
+    },
+  },
 }
+
 const App = () => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("")
 
   return (
     <ReactQuill defaultValue={value} onChange={setValue} modules={modules} />
