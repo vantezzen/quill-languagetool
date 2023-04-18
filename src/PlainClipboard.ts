32acc19f3ab820594906ab02a10d54247effbec9
Larissa <larissa.tardivo@estantemagica.com.br>
2023-04-18 09:30:13 -0300

    fix: replace clipboard text on quill

---
+++ src/PlainClipboard.ts
@@ -0,0 +1,20 @@
+import Quill from "quill"
+const Clipboard = Quill.import("modules/clipboard")
+const Delta = Quill.import("delta")
+
+export default class PlainClipboard extends Clipboard {
+  onPaste(e: any) {
+    e.preventDefault()
+    const range = this.quill.getSelection()
+    const text = e.clipboardData.getData("text/plain")
+    const delta = new Delta()
+      .retain(range.index)
+      .delete(range.length)
+      .insert(text)
+    const index = text.length + range.index
+    const length = 0
+    this.quill.updateContents(delta, "silent")
+    this.quill.setSelection(index, length, "silent")
+    this.quill.scrollIntoView()
+  }
+}
